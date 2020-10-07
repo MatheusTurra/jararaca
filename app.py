ea45996c594150cf20503eba17f8cd349fd4d093
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2020-10-07 11:38:28 -0300

    fix files format

--- app.py
+++ app.py
@@ -9,13 +9,16 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 
+
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def home_page():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
@@ -29,11 +32,12 @@ def upload_page():
             extracted_text = ocr_core(file)
             # print(UPLOAD_FOLDER + file.filename)
             return render_template('upload.html',
-                                    msg= 'Processada com sucesso',
-                                    extracted_text=extracted_text,
-                                    img_src=UPLOAD_FOLDER + file.filename)
+                                   msg='Processada com sucesso',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
     elif request.method == 'GET':
         return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run()
--- ocr_core.py
+++ ocr_core.py
@@ -4,6 +4,7 @@ except ImportError:
     import Image
 import pytesseract
 
+
 def ocr_core(filename):
     text = pytesseract.image_to_string(Image.open(filename))
     return text
